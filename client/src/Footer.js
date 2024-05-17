018318a6f3dc56ef0e99f0eb866b0895d9d5af27
Vibhu Bhardwaj <97392063+Vibhu-Bhardwaj@users.noreply.github.com>
2024-05-17 14:33:53 +0530

    Update Footer.js

--- client/src/Footer.js
+++ client/src/Footer.js
@@ -15,7 +15,7 @@ export default function Footer() {
     >
       <section className=" d-flex justify-content-center justify-content-lg-between p-4 border-bottom">
         <div className="me-5 d-none d-lg-block">
-          <span>Get connected with Vibhu on social networks:</span>
+          <span>Get connected with us on social networks:</span>
         </div>
 
         <div className="text-dark">
