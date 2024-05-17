e81a16f9d73bfa7156be9f1fd74f0d6f1b28b745
Vibhu Bhardwaj <97392063+Vibhu-Bhardwaj@users.noreply.github.com>
2024-05-17 18:54:09 +0530

    Update resetpass.js

--- client/src/PasswordResetComponents/resetpass.js
+++ client/src/PasswordResetComponents/resetpass.js
@@ -1,13 +1,11 @@
 import React, { useState } from 'react';
 import axios from 'axios';
-import { useHistory } from 'react-router-dom';
 import { useToast } from "@chakra-ui/react";
 
 function ResetPassword() {
   const [password, setPassword] = useState('');
   const [confirmPassword, setConfirmPassword] = useState('');
   const toast = useToast();
-  const history = useHistory();
 
   const handleResetPassword = (event) => {
     event.preventDefault();
@@ -35,8 +33,6 @@ function ResetPassword() {
           duration: 5000,
           isClosable: true,
         });
-        // Redirect to home page after successful password reset
-        history.push("/");
       })
       .catch((error) => {
         toast({
